cbcaa3c12c044935618a4bc6fcf931e520b77709
Markus Towara <mtowara@gmail.com>
2016-09-01 20:11:55 +0200

    misc cleanup

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ plugin_package = "octoprint_GCodeBar"
 plugin_name = "OctoPrint-Gcodebar"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.1"
+plugin_version = "0.1.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = "Plugin for Octoprint to allow sending GCode commands from the sidebar"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Markus Towara"
